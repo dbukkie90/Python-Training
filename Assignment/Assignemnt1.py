7d5f74fea51825154b8d33657c2c91d92efc44d5
dbukkie90 <57272021+dbukkie90@users.noreply.github.com>
2020-04-05 15:14:49 -0700

    Create Assignemnt1.py

---
+++ Assignment/Assignemnt1.py
@@ -0,0 +1,123 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+'''
+first_n = 5
+second_n = 7
+third_n = 10
+result = first_n + second_n + third_n
+print (result)
+
+initial = "Ade"
+first_n = "Sam"
+first_n1 = 2
+first_n2 = 2.5
+res = initial + first_n
+print (res)
+res = res + str(first_n1)
+print (res)
+'''
+
+'''
+print ("Enter the first number")
+x = int (input())
+print ("Enter the second number")
+y = int (input())
+print ("Enter the third number")
+z = int (input())
+result = (x+y+z)/2
+print (result)
+'''
+
+'''
+a = 5
+b = 10
+a>b
+print (a>b)
+print (a<b)
+print (a>=b)
+print (b<=a)
+print (a!=b)
+print (a==b) 
+
+print ("Hello World!")
+'''
+
+'''
+Elect101 = 5
+Elect102 = 10
+if (Elect101 > Elect102):
+    print("Good grade")
+else:
+     print("Bad grade")
+if (Elect101 != 15):
+    print ("Non standard grade")
+    
+'''
+
+#print ("I love you Taiwo")
+
+#elif
+
+'''
+2+2
+print (2+2)
+'''
+
+#print ("2+2")
+
+#print ("Hello " + "Ladies " + "Learning " + "Code")
+
+'''
+x = 5
+y = 4
+x*y
+print (x*y)
+'''
+
+
+'''
+animal = "dog"
+print ("my favourite animal is a " + animal)
+'''
+
+'''
+a = "sam" + "jo"
+print (a)
+'''
+
+#print ('I have eaten ' + str(99) + ' burritos.')
+
+#print ("spam" + "spamspam")
+
+#print ("spam" * 3)
+
+'''
+print ("Enter the first number")
+a = int (input())
+print ("Enter the second number")
+b = int (input())
+print ("Enter the third number")
+c = int (input())
+print ("Enter the fourth number")
+d = int (input())
+print ("Enter the fifth number")
+e = int (input())
+result = (a+b+c+d+e)/2
+print (result)
+if (result > 60):
+    print("success")
+else:
+     print("failure")
+if (result == 50):
+    print ("I am equal")
+else:
+    print ("Not equal")
+if (result>=51) and (result <=60):
+    print ("I am inbetween")
+'''
+    
