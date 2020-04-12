ae98e3fe0da6d527db59dc3ff6275f02b9e6004d
dbukkie90 <57272021+dbukkie90@users.noreply.github.com>
2020-04-12 14:04:35 -0700

    Add files via upload

--- Lecture 3 Assignment.py
+++ Lecture 3 Assignment.py
@@ -5,7 +5,7 @@ Created on Sun Apr 12 06:36:19 2020
 @author: dbukk
 """
 
-
+'''
 #Assignment
 
 #Area of a Circle = πr^2
@@ -18,9 +18,9 @@ def circleArea (r):
     print (area)
 r=6
 circleArea (r)
+
+
 '''
-
-
 #Area of Rectangle = l*b
 '''
 def rectangleArea (l,b):
@@ -29,24 +29,25 @@ def rectangleArea (l,b):
 l = 4
 b = 6
 rectangleArea (l,b)
-'''
 
+
+'''
 #Area of a Square = l*l
 '''
 side = int(input("Enter side of square:"))
 area = side*side
 print ("Area of square = ", area)
+
+
 '''
-
-
 #Area of cube = 6a**2
 '''
 surface_area = int(input("Enter surface area of cube:" ))
 area = 6*surface_area**2
 print  ("Area of cube = ", area)
+
+
 '''
-
-
 #Area of Cylinder = 2πrh + 2πr**2
 '''
 import math
@@ -56,10 +57,11 @@ h = 9
 
 area = 2* π * r * h + 2 * π * r**2
 print ("Area of cylinder = ", area)
+
+
 '''
-
-
 #Area of a Sector in degrees = (θ/360) * π * r**2
+'''
 import math
 def SectorArea ():
     r = float(input("Enter radius of Circle:")) 
