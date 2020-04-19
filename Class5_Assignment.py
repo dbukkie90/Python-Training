abd7246c1752be97d8bf779c450e179af274fc5f
Dami <57272021+dbukkie90@users.noreply.github.com>
2020-04-19 06:10:20 -0700

    Add files via upload

---
+++ Class5_Assignment.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 19 06:04:55 2020
+
+@author: dbukk
+"""
+
+
+data = [7,8, 4, 6,3, 8, 9]
+
+'''
+max_data = max(data)
+print (max_data)
+'''
+
+'''
+Min_data = min(data)
+print (Min_data)
+'''
+
+'''
+maxData = data [0]
+for i in range (0, len(data), 1):
+    maxData = max(maxData, data[i])
+print(maxData)
+'''
+
+'''
+min_data = data[0]
+for i in range (0, len(data), 1):
+    min_data = min(min_data, data[i])
+print (min_data)
+'''
+
